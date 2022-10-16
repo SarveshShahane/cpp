6ae931dda8d9d3615e37f681b5132a74484512f5
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-10-16 22:32:40 +0530

    two.cpp

--- two.cpp
+++ two.cpp
@@ -4,5 +4,17 @@ int main(){
   cout<<"Enter a b c "<<endl;
   cin>>a>>b>>c;
   cout<<a<<b<<c;
+  if(a>b&&a>c)
+  {
+    cout<<"A is max"<<endl;
+  }
+  else if(b>a&&b>c)
+  {
+    cout<<"B is max"<<endl;
+  }
+  else if(c>a&&c>b)
+  {
+    cout<<"C is max"<<endl;
+  }
   return 0;
 }
